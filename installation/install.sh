267856735352f5aaf2113f31f691838b492f165e
Jacky <jackylai@foxmail.com>
2020-10-28 10:19:46 -0400

    Included compilation into installation script

--- installation/install.sh
+++ installation/install.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+if [ $machine != "Mac" -o $machine != "Linux" ]
+then
+    echo -e "not supported"
+    exit 1
+fi
 install_dirname="install_aslCalc"
 mkdir $install_dirname
 cd $install_dirname
-wget https://raw.githubusercontent.com/JackyKLai/asl_calculator/main/bin/asl_calculator
+wget https://raw.githubusercontent.com/JackyKLai/asl_calculator/main/bin/asl_calculator.c
+if [ $machine == "Mac" ]
+then 
+    clang -o asl_calculator asl_calculator.c
+else
+    gcc -o asl_calculator asl_calculator.c
+fi
 chmod a+x ./asl_calculator
 sudo mv ./asl_calculator /usr/local/bin
+rm ./asl_calculator.c
 cd ..
 rmdir $install_dirname
 echo done
