f6be3db169ea2a8b1e896148bdc0af3766fbe175
Jacky <jackylai@foxmail.com>
2020-10-27 22:21:08 -0400

    Added installation files

---
+++ installation/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+install_dirname="install_aslCalc"
+mkdir $install_dirname
+cd $install_dirname
+wget https://raw.githubusercontent.com/JackyKLai/asl_calculator/main/bin/asl_calculator
+chmod a+x ./asl_calculator
+sudo mv ./asl_calculator /usr/local/bin
+cd ..
+rmdir $install_dirname
+echo done
